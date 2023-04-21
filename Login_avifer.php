54235fa4ec8635ea037c079bf448b02fa1a53b55
Hailtom Garcia Dias <122056292+ttrikingG@users.noreply.github.com>
2023-04-20 21:55:25 -0300

    Add files via upload

---
+++ Login_avifer.php
@@ -0,0 +1,112 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Avifer</title>
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+</head>
+<body>
+    <div class="iconsusp">
+        <a class="telefone" href="#">Para entrar em contato ligue:(12) 3916-4820</a>
+        <a href="#" target="_blank"><img src="../Avifer_loja/Icons/icon-whatsapp-48.png"></a>
+        <a href="#" target="_blank"><img src="../Avifer_loja/Icons/icon-telegram-50.png"></a>
+        <a href="#" target="_blank"><img src="../Avifer_loja/Icons/fb_icons.png"></a>
+    </div> <!--links para social mídia-->
+
+<header>
+    <img src="img/Avifer-Banner-Loja.png" alt="logo da loja" title="Avifer logo">
+</header>
+
+    <div class="topnav"> <!--menu de navegação principal-->
+        <a href="index.php">Início</a>
+        <a href="Login_avifer.php">Contato</a>
+        <a href="https://www.facebook.com/avifer2912" target="_blank">Mídia</a>
+        <div class="search_container">
+            <!--<form action="#" method="post">
+                <input type="text" name="search" placeholder="Pesquisa" required>
+                <button type="submit">Buscar</button>
+            </form>-->
+        </div>
+    </div>
+
+    <!--<div class="tag-categorias">sub menu
+        <ul>
+            <li><a href="">Pesca</a></li>
+            <li><a href="">Rações</a></li>
+            <li><a href="">Aquarismo</a></li>
+            <li><a href="">Jardinagem</a></li>
+            <li><a href="">Vacinas</a></li>
+            <li><a href="">Aves</a></li>
+        </ul>
+    </div>-->
+
+    <form class="avifer-form"action=""><!--inicio formulário login-->
+        <img class="logo-form" src="img/Avifer-logo-3.png" height="54px" width="96px" alt="">
+        <h1 class="titulo-form">Contato</h1>
+        <p>Acesse Abaixo</p>
+
+        <div class="menuft1a">
+            <a href="#" target="_blank"><img src="../Avifer_loja/Icons/icon-whatsapp-48.png"></a>
+        </div><!--links para social mídia-->
+
+        <label class="rotulo-form" for="">
+            <img class="icon-form" src="Icons/email-icone2.png" alt="">
+            <input class="input-form" type="text" placeholder="E-mail">
+        </label>
+        
+        <label class="rotulo-form" for="">
+            <img class="icon-form" src="Icons/Lapis-icon.png" alt="">
+            <input class="input-form" type="text" placeholder="Assunto">
+        </label>
+
+        <label class="rotulo-form" for="">
+            <img class="icon-form" src="Icons/Lapis-icon.png" alt="">
+			<textarea class="input-form" placeholder="Mensagem"></textarea>
+        </label>
+
+        <button class="btn-form">Enviar</button>
+
+        <p class="texto-form">Entre em contato pelo telefone:(12) 3916-4820</p>
+    </form><!--inicio formulário login-->
+   
+    <footer class="ft1"><!--início Rodapé-->
+        <div class="ft2">
+            <ul class="menuft2a">
+                <li><a href="index.php">Início</a></li>
+                <li><a href="#">Produtos</a></li>
+                <li><a href="#">Contato</a></li>
+                <li><a href="#">Serviços</a></li>
+                <li><a href="#">Sobre</a></li>
+                <li><a href="#" target="_blank">Mídia</a></li>
+            </ul>
+
+            <ul class="menuft2b">
+                <li><a href="#">Equipamentos para Pesca</a></li>
+                <li><a href="#">Aves</a></li>
+                <li><a href="#">Rações</a></li>
+                <li><a href="#">Jardinagem</a></li>
+                <li><a href="#">Aquarismo</a></li>
+                <li><a href="#">Medicamentos Veterinários</a></li>
+            </ul>
+
+            <ul class="menuft2c">
+                <li><a href="#">(12) 3916-4820</a></li>
+            </ul>
+        </div> <!--menu com todos os botões do site-->
+
+        <div class="menuft1a">
+            <a href="#" target="_blank"><img src="../Avifer_loja/Icons/icon-whatsapp-48.png"></a>
+            <a href="#" target="_blank"><img src="../Avifer_loja/Icons/icon-telegram-50.png"></a>
+            <a href="#" target="_blank"><img src="../Avifer_loja/Icons/fb_icons.png"></a>
+        </div> <!--links para social mídia-->
+
+        <div class="menucp">
+            <p>Avenida Andrômeda, 2912, São José dos Campos, SP, Brazil.</p>
+            <p>© Copyright: Avifer by ttricking</p>
+        </div>
+    </footer>
+<script src="../Avifer_loja/script/script.js"></script>
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,300 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Avifer</title>
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+</head>
+<body>
+    <div class="iconsusp">
+        <a class="telefone" href="#">Entre em contato:(12) 3916-4820</a>
+        <a href="#" target="_blank"><img src="../Avifer_loja/Icons/icon-whatsapp-48.png"></a>
+        <a href="#" target="_blank"><img src="../Avifer_loja/Icons/icon-telegram-50.png"></a>
+        <a href="https://www.facebook.com/avifer2912" target="_blank"><img src="../Avifer_loja/Icons/fb_icons.png"></a>
+        <!--<div class="btn_logintop">
+            <a class="btn_enter" href="cadastro.php">Entrar</a>/<a class="btn_sair" href="logoff.php">Sair</a>
+        </div>-->
+    </div> <!--links para social mídia-->
+
+<header>
+    <img src="img/Avifer-Banner-Loja.png" alt="logo da loja" title="Avifer logo">
+</header>
+
+    <div class="topnav"> <!--menu de navegação principal-->
+        <a href="index.php">Início</a>
+        <a href="#servicos-anchor">Serviços</a>
+        <a href="#parceiros-anchor">Parceiros</a>
+        <a href="#local-anchor">Localização</a>
+        <a href="Login_avifer.php">Contato</a>
+        <a href="https://www.facebook.com/avifer2912" target="_blank">Mídia</a>
+        <div class="search_container">
+            <!--<form action="#" method="post">
+                <input type="text" name="search" placeholder="Pesquisa" required>
+                <button type="submit">Buscar</button>
+            </form>-->
+        </div>
+    </div>
+
+    <!--<div class="tag-categorias">sub menu
+        <ul>
+            <li><a href="">Pesca</a></li>
+            <li><a href="">Rações</a></li>
+            <li><a href="">Aquarismo</a></li>
+            <li><a href="">Jardinagem</a></li>
+            <li><a href="">Vacinas</a></li>
+            <li><a href="">Aves</a></li>
+        </ul>
+    </div>-->
+
+    <div class="slideshow-container"><!--inicio slide show abaixo do menu-->
+        
+        <div class="mySlides fade">
+          <div class="numbertext">1 / 5</div>
+          <img src="../Avifer_loja/img/interior_loja6.jpg.jpg" style="width:100%">
+          
+        </div>
+
+        <div class="mySlides fade">
+          <div class="numbertext">2 / 5</div>
+          <img src="../Avifer_loja/img/interior_loja2.jpg" style="width:100%">
+          
+        </div>
+        
+        <div class="mySlides fade">
+          <div class="numbertext">3 / 5</div>
+          <img src="../Avifer_loja/img/interior_loja4.jpg" style="width:100%">
+          
+        </div>
+
+        <div class="mySlides fade">
+          <div class="numbertext">4 / 5</div>
+          <img src="../Avifer_loja/img/interior_loja3.jpg" style="width:100%">
+          
+        </div>
+
+        <div class="mySlides fade">
+          <div class="numbertext">5 / 5</div>
+          <img src="../Avifer_loja/img/interior_loja5.jpg" style="width:100%">
+          
+        </div>
+        
+        <a class="prev" onclick="plusSlides(-1)">❮</a>
+        <a class="next" onclick="plusSlides(1)">❯</a>
+        
+        </div>
+        <br>
+        
+        <div style="text-align:center">
+          <span class="dot" onclick="currentSlide(1)"></span> 
+          <span class="dot" onclick="currentSlide(2)"></span> 
+          <span class="dot" onclick="currentSlide(3)"></span>
+          <span class="dot" onclick="currentSlide(4)"></span> 
+          <span class="dot" onclick="currentSlide(5)"></span> 
+        </div>
+    </div><!--FIM do slide show abaixo do menu-->
+
+    <div class="Banner-Serviços"><!--Banner de Serviços-->
+        <h1><a name="servicos-anchor">Serviços</a></h1>
+        <img class="img-servicos" src="../Avifer_loja/img/Serviços_avifer.png" alt="">
+    </div>
+
+    <div class="banner-produtos">
+        <h1><a name="parceiros-anchor">Parceiros</a></h1>
+    </div>
+
+    <div class="container-body"><!--INÍCIO vitrine da loja-->
+            <div class="body-lateral">
+                <div class="container-card">
+                    <div class="card1">
+                        <img class="img-vendas" src="img/Crown-logo.png" alt="">
+                    </div>
+
+                    <h1>Crown</h1>
+        
+                    <!--<div class="btn-do-card">
+                        <button class="btn-card1">Sobre</button>
+                        <button class="btn-card2">Comprar</button>
+                    </div>-->
+                </div>
+            </div>
+
+            <div class="body-lateral">
+                <div class="container-card">
+                    <div class="card1">
+                        <img class="img-vendas" src="img/Saint-logo.png" alt="">
+                    </div>
+
+                    <h1>Saint</h1>
+                   
+                    <!--<div class="btn-do-card">
+                        <button class="btn-card1">Sobre</button>
+                        <button class="btn-card2">Comprar</button>
+                    </div>-->
+                </div>
+            </div>
+
+            <div class="body-lateral">
+                <div class="container-card">
+                    <div class="card1">
+                        <img class="img-vendas" src="img/Joga-logo.png" alt="">
+                    </div>
+
+                    <h1>Jogá</h1>
+
+                    <!--<div class="btn-do-card">
+                        <button class="btn-card1">Sobre</button>
+                        <button class="btn-card2">Comprar</button>
+                    </div>-->
+                </div>
+            </div>
+
+            <div class="body-lateral">
+                <div class="container-card">
+                    <div class="card1">
+                        <img class="img-vendas" src="img/AF-logo.png" alt="">
+                    </div>
+
+                    <h1>Albatroz-Fishing</h1>
+                    
+                    <!--<div class="btn-do-card">
+                        <button class="btn-card1">Sobre</button>
+                        <button class="btn-card2">Comprar</button>
+                    </div>-->
+                </div>
+            </div>
+
+            <div class="body-lateral">
+                <div class="container-card">
+                    <div class="card1">
+                        <img class="img-vendas" src="img/wiskas-logo.png" alt="">
+                    </div>
+
+                    <h1>Whiskas</h1>
+
+                    <!--<div class="btn-do-card">
+                        <button class="btn-card1">Sobre</button>
+                        <button class="btn-card2">Comprar</button>
+                    </div>-->
+                </div>
+            </div>
+
+            <div class="body-lateral">
+                <div class="container-card">
+                    <div class="card1">
+                        <img class="img-vendas" src="img/Pedigree-logo.png" alt="">
+                    </div>
+
+                    <h1>Pedigree</h1>
+
+                    <!--<div class="btn-do-card">
+                        <button class="btn-card1">Sobre</button>
+                        <button class="btn-card2">Comprar</button>
+                    </div>-->
+                </div>
+            </div>
+
+            <div class="body-lateral">
+                <div class="container-card">
+                    <div class="card1">
+                        <img class="img-vendas" src="img/specialdog-logo.png" alt="">
+                    </div>
+
+                    <h1>Special-dog</h1>
+
+                    <!--<div class="btn-do-card">
+                        <button class="btn-card1">Sobre</button>
+                        <button class="btn-card2">Comprar</button>
+                    </div>-->
+                </div>
+            </div>
+
+            <div class="body-lateral">
+                <div class="container-card">
+                    <div class="card1">
+                        <img class="img-vendas" src="img/dogmil-logo.png" alt="">
+                    </div>
+
+                    <h1>Dogmil</h1>
+                    
+                    <!--<div class="btn-do-card">
+                        <button class="btn-card1">Sobre</button>
+                        <button class="btn-card2">Comprar</button>
+                    </div>-->
+                </div>
+            </div>
+        </div><!--FIM vitrine da loja-->
+
+    <div class="menupdt">
+        <h1>Categorias</h1>
+        <main>
+            <div class="gallery-container">
+                <a href="../Avifer/index.html" class="gallery-itens" target="_blank">
+                    <img src="../Avifer_loja/img/btn-Avifer1.png" alt="Avifer">  
+                </a>
+                <a href="" class="gallery-itens">
+                    <img src="../Avifer_loja/img/btn-Avifer4.png" alt="">
+                </a>
+                <a href="" class="gallery-itens">
+                    <img src="../Avifer_loja/img/btn-Avifer3.png" alt="">
+                </a>
+                <a href="" class="gallery-itens">
+                    <img src="../Avifer_loja/img/btn-Avifer5.png" alt="">
+                </a>
+                <a href="" class="gallery-itens">
+                    <img src="../Avifer_loja/img/btn-Avifer2.png" alt="">
+                </a>
+                <a href="" class="gallery-itens">
+                    <img src="../Avifer_loja/img/btn-Avifer6.png" alt="">
+                </a>
+            </div>
+        </main>
+    </div>
+
+    <div class="mapa-local">
+        <h1><a name="local-anchor"></a>Localização</h1>
+        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3666.134928843831!2d-45.88738508555566!3d-23.238176584844307!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94cc4ab874d8cea7%3A0x25211930bdfbf1d7!2sAvifer%20Com%C3%A9rcio%20de%20Aves%20e%20Ra%C3%A7%C3%B5es%2C%20material%20de%20pesca%2C%20jardinagem%2C%20vacina%2C%20medicamentos%20entre%20outros...!5e0!3m2!1spt-BR!2sbr!4v1680893263236!5m2!1spt-BR!2sbr"
+             margin="auto" width="100%" height="320" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+    </div>
+
+           
+    <footer class="ft1"><!--início Rodapé-->
+        <div class="ft2">
+            <ul class="menuft2a">
+                <li><a href="index.php">Início</a></li>
+                <li><a href="#parceiros-anchor">Parceiros</a></li>
+                <li><a href="Login_avifer.php">Contato</a></li>
+                <li><a href="#servicos-anchor">Serviços</a></li>
+                <li><a href="#local-anchor">Localização</a></li>
+                <li><a href="https://www.facebook.com/avifer2912" target="_blank">Mídia</a></li>
+            </ul>
+
+            <ul class="menuft2b">
+                <li><a href="#">Equipamentos para Pesca</a></li>
+                <li><a href="#">Aves</a></li>
+                <li><a href="#">Rações</a></li>
+                <li><a href="#">Jardinagem</a></li>
+                <li><a href="#">Aquarismo</a></li>
+                <li><a href="#">Medicamentos Veterinários</a></li>
+            </ul>
+
+            <ul class="menuft2c">
+                <li><a href="#">(12) 3916-4820</a></li>
+            </ul>
+        </div> <!--menu com todos os botões do site-->
+
+        <div class="menuft1a">
+            <a href="#" target="_blank"><img src="../Avifer_loja/Icons/icon-whatsapp-48.png"></a>
+            <a href="#" target="_blank"><img src="../Avifer_loja/Icons/icon-telegram-50.png"></a>
+            <a href="https://www.facebook.com/avifer2912" target="_blank"><img src="../Avifer_loja/Icons/fb_icons.png"></a>
+        </div> <!--links para social mídia-->
+
+        <div class="menucp">
+            <p>Avenida Andrômeda, 2912, São José dos Campos, SP, Brazil.</p>
+            <p>© Copyright: Avifer by ttricking</p>
+        </div>
+    </footer>
+<script src="../Avifer_loja/script/script.js"></script>
+</body>
+</html>
